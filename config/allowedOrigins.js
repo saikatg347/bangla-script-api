bc19089b3349f8a0f094fc54e4ded43c13bb17b8
Saikat Das <saikat.g347@gmail.com>
2024-02-28 18:09:40 +0530

    added new cors origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5500']
+const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5500', 'https://saikatg347.github.io']
 
 module.exports = allowedOrigins
